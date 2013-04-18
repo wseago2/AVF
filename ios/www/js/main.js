0863af8616f385ac9adcea4c124059fe4e522c09
Wesley Seago <wseago2@fullsail.edu>
2013-04-17 23:58:42 -0400

    JS done.

---
+++ ios/www/js/main.js
@@ -0,0 +1,83 @@
+$("document").ready (function () {
+	
+	getTwitter();
+	getWeather();
+	
+	
+});
+
+var getTwitter = function (){
+	$(".twitterHeader").remove();
+	var maketwitterHeader = $('<h2 class="twitterHeader">Recent Astronomy Tweets</h2>');
+	maketwitterHeader.appendTo('#twitterHeader');
+	$.ajax({
+		url: 'http://search.twitter.com/search.json?q=celestron&rpp=5&include_entities=true&result_type=mixed',
+		type: 'GET',
+		dataType: 'jsonp',
+		success: function (tdata){
+			console.log(tdata);
+				var currentTweets = $();
+				currentTweets.appendTo('#twitterHeader');
+				for (i=0, l=tdata.results.length; i<l; i++){
+					$(' ' +
+						'<ul class="twitterList">' +
+							'<li><h3>' + tdata.results[i].from_user + ' ' + '</h3></li>' +
+							'<li>' + tdata.results[i].from_user_name + ' ' + '</li>' +
+							'<li>' + tdata.results[i].text + ' ' + '</li>' +
+							'</ul>'
+						).appendTo('#twitterHeader');
+				}
+
+				
+		}
+	})
+};
+
+var getWeather = function (){
+	$(".weatherHeader").remove();
+		var makeweatherHeader = $('<h2 class="weatherHeader">Weather</h2>');
+		var makeSub = $('<h4 class="weatherHeader">Orlando, FL</h4>');
+		makeweatherHeader.appendTo('#weatherHeader');
+		makeSub.appendTo('#weatherHeader');
+			$.ajax({
+				url: 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=Orlando&format=json&num_of_days=5&key=ugp5sknf56rtzf69nt5p59rx',
+				type: 'GET',
+				dataType: 'jsonp',
+				success: function (weatherData){
+					console.log(weatherData);					
+					var currentCondition = $();
+					currentCondition.appendTo('#weatherHeader');
+					for (i=0, l=weatherData.data.current_condition.length; i<l; i++){						
+							$(' ' +
+									'<ul class="weatherList">' +
+										'<li> Current Conditions: ' + weatherData.data.current_condition[i].weatherDesc[i].value + '</li>' +
+										'<li> Temperature: ' + weatherData.data.current_condition[i].temp_F + 'F' + '</li>' +
+										'<li> Humidity: ' + weatherData.data.current_condition[i].humidity + '%' + '</li>' +
+										'<li> Precipitation: ' + weatherData.data.current_condition[i].precipMM + '"' + '</li>' +
+										'<li> Barometric Pressure: ' + weatherData.data.current_condition[i].pressure + 'mb' + '</li>' +
+										'<li> Cloud Cover: ' + weatherData.data.current_condition[i].cloudcover + '%' + '</li>' +
+										'<li> Visibility: ' + weatherData.data.current_condition[i].visibility + 'miles' + '</li>' +
+									'</ul>'
+								).appendTo('#weatherHeader');
+						}
+					}					
+			})
+};
+
+/* Weather API Notes
+	If I assign a variable to the URL
+	I can use a zipcode instead of a 
+	city name and allow the user to 
+	enter their own zip code, so that
+	the weather report will be for them.
+*/	
+
+/*
+	for (i=0, l=weatherData.data.current_condition.length; i<l; i++){
+		alert(i);
+		alert(weatherData.data.current_condition[i].temp_F);
+		$(".weatherList").remove();
+		$(".twitterList").remove();
+*/
+
+
